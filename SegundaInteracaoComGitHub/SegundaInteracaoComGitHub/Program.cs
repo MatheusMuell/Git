890970d0f06b68dffffbff5b5b126069cf5333d5
Matheus Mueller <matheus_mueller06@hotmail.com>
2019-07-13 16:06:42 -0300

    Segunda interação com o GitHub

---
+++ SegundaInteracaoComGitHub/SegundaInteracaoComGitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundaInteracaoComGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Segundo projeto
+            Console.WriteLine("Utilizando o Git");
+        }
+    }
+}
