13bece753968cab172d8d8c97b672ee2240c8de4
Matheus Mueller <matheus_mueller06@hotmail.com>
2019-07-13 15:46:30 -0300

    Primeiro contato com o GitHub

---
+++ PrimeiroContatoComGitHub/PrimeiroContatoComGitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroContatoComGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git!");
+        }
+    }
+}
